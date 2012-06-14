d9b2c4244902fa947931e63bbdc58d68c59c6555
Kaplux <kaplux@gmail.com>
2012-06-14 21:04:52 +0200

    v1.2

--- src/main/java/fr/mildlyusefulsoftware/thefunniest/activity/ViewPictureActivity.java
+++ src/main/java/fr/mildlyusefulsoftware/thefunniest/activity/ViewPictureActivity.java
@@ -9,6 +9,7 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import android.os.Bundle;
 import android.util.Log;
 import fr.mildlyusefulsoftware.imageviewer.service.DatabaseHelper;
 import fr.mildlyusefulsoftware.imageviewer.service.Picture;
@@ -46,5 +47,41 @@ public class ViewPictureActivity extends fr.mildlyusefulsoftware.imageviewer.act
 			e.printStackTrace();
 		}
 	}
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+//		putNewPictures();
+	}
+	
+	private void putNewPictures(){
+		String[] url =new String[]{"http://www.dvo.com/newsletter/monthly/2003/october/FunnyVancouver.jpg","http://img375.imageshack.us/img375/4331/sealfunnyoj6.jpg",
+				"http://students.cs.byu.edu/~appleman/images/funny/t.jpg","http://i18.photobucket.com/albums/b123/actress47/DSC00566.jpg",
+				"http://www.worth1000.com/entries/18000/18262.jpg",
+					"http://www.myconfinedspace.com/wp-content/uploads/2006/10/japanese-frog.thumbnail.jpg",
+					"http://img50.imageshack.us/img50/1330/geekql6.jpg",
+					"http://halbot.haluze.sk/images/2006-07/2425_flickrdeath.jpg",
+					"http://mirror.servut.us/kuvat/motivation/u_talking_to_me.jpg",
+					"http://i.thefairest.info/funniest_thumbs/3lZtUy.jpeg",
+					"http://www.phun.org/newspics/funny_friday/1914.jpg",
+					"http://homepage.ntlworld.com/tonystrading/pix/just_for_fun/mousetrap.jpg"		
+					
+		};
+		int i=40;
+		List<Picture> pictures=new ArrayList<Picture>();
+		for (int j=0;j<url.length;j++){
+			try {
+				Log.i("thefunniest",url[j]);
+				Picture p = new Picture(j+i, url[j],Picture.getPictureThumbnail(url[j]));
+				pictures.add(p);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		DatabaseHelper.connect(this).putPictures(pictures);
+	}
+
 
 }
